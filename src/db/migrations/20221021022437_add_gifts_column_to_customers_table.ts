c310346b3cdc7eb0fbec047b4c2f3490b8ab2d06
Can Toraman <toraman@usc.edu>
2022-10-21 01:06:20 -0700

    gifts plural typo fix in knex migration

--- src/db/migrations/20221021022437_add_gifts_column_to_customers_table.ts
+++ src/db/migrations/20221021022437_add_gifts_column_to_customers_table.ts
@@ -2,11 +2,11 @@ import { Knex } from "knex";
 
 export const up = (knex: Knex) => {
   return knex.schema.alterTable("customers", (table) => {
-    table.string("gifts");
+    table.string("gift");
   });
 };
 
 export const down = (knex: Knex) =>
   knex.schema.alterTable("customers", (table) => {
-    table.dropColumn("gifts");
+    table.dropColumn("gift");
   });
